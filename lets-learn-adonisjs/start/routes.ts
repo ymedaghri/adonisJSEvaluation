1d0b0c625f9a9694d2dae2ce45ed6e8af44665f3
Medaghri-Alaoui Youssef <ymedaghri@gmail.com>
2026-02-01 14:51:54 +0100

    feat: 🎸 Routing
    Adding routing examples

--- lets-learn-adonisjs/start/routes.ts
+++ lets-learn-adonisjs/start/routes.ts
@@ -7,6 +7,28 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/movies/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    let movie
+
+    try {
+      movie = await fs.readFile(url, 'utf-8')
+    } catch (error) {
+      throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
+    }
+
+    return ctx.view.render('pages/movies/show', { movie })
+  })
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
